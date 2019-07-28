fbd1dbbfd1d1dd32811b86b9d5521e4022ea3f53
hman523 <shbarbella@gmail.com>
2019-07-28 18:00:40 -0400

    Used hlint and fixed the hints it offered

--- med.hs
+++ med.hs
@@ -1,4 +1,4 @@
-import Prelude hiding (lookup)
+import Prelude
 import System.Environment
 import System.Exit
 import System.IO
@@ -7,6 +7,7 @@ import Data.Text as Tx (Text, pack, unpack, take, takeEnd, drop)
 import Data.Text.Internal.Search
 import Data.List
 import Data.Maybe
+import Control.Monad
 
 splitchars = " ()[]{};\n"
 --getIndexesHelper
@@ -14,27 +15,24 @@ gIH :: String -> Char -> [Int]
 gIH text delim = indices (Tx.pack $ (:[]) delim) (Tx.pack text)
 getIndexes :: String -> String -> [Int]
 getIndexes _ "" = []
-getIndexes text delims = ((gIH text (head delims)) ++ (getIndexes text (tail delims)))
+getIndexes text delims = gIH text (head delims) ++ getIndexes text (tail delims)
 
 --https://stackoverflow.com/questions/48369242/in-haskell-how-can-i-get-a-substring-of-a-text-between-two-indices
---slice :: Int -> Int -> Text -> Text
---slice a b text = takeEnd a (Tx.take b text)
-
 substring :: Int -> Int -> Text -> Text
 substring start end text = Tx.take (end - start) (Tx.drop start text)
 
 splitByIndexes :: String -> [Int] -> [String]
-splitByIndexes _ ([]) = []
-splitByIndexes _ (x:[]) = []
-splitByIndexes str lst = (Tx.unpack (substring ((head lst) + 1) (lst !! 1) (Tx.pack str))) : (Tx.unpack $ substring (lst !! 1) ((lst !! 1) + 1) (Tx.pack str))  : splitByIndexes str ((tail lst))
+splitByIndexes _ [] = []
+splitByIndexes _ [x] = []
+splitByIndexes str lst = Tx.unpack (substring (head lst + 1) (lst !! 1) (Tx.pack str)) : Tx.unpack (substring (lst !! 1) ((lst !! 1) + 1) (Tx.pack str))  : splitByIndexes str (tail lst)
 
 parseCode :: String -> [String]
 parseCode "" = []
-parseCode str = splitByIndexes str ([-1] ++ (((sort (getIndexes str splitchars)))) ++ [(length str)])
+parseCode str = splitByIndexes str ((-1) : sort (getIndexes str splitchars) ++ [length str])
 
-itToEngDict = Map.fromList([
+itToEngDict = Map.fromList[
   ("se", "if"), 
-  ("per", "for")])
+  ("per", "for")]
 
 translateItToEng :: String -> String
 translateItToEng x = if Map.member x itToEngDict
@@ -43,12 +41,14 @@ translateItToEng x = if Map.member x itToEngDict
 
 main = do
   args <- getArgs
-  if length args /= 1 
-    then exitFailure 
-	else putStrLn $ "Compiling file " ++ head args
+  when (length args /= 1) $ 
+      do 
+	    putStrLn "Need a file to compile"
+	    exitFailure 
   let filename = head args
   contents <- readFile filename
-  let output = concat (map translateItToEng (parseCode contents))
+  let output = concatMap translateItToEng $ parseCode contents
   writeFile (filename ++ ".c") output
   --If you reach this point, exit successfully
+  putStrLn ("Successfully compiled " ++ filename ++ "!")
   exitSuccess 
