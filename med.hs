65ccbea6d4327bbff4d9e07ecb35b6a76fa5e950
hman523 <shbarbella@gmail.com>
2019-07-28 21:15:44 -0400

    Added all the keywords

--- med.hs
+++ med.hs
@@ -10,6 +10,8 @@ import Data.Maybe
 import Control.Monad
 
 splitchars = " ()[]{};\n"
+
+--getIndexes: returns a list of ints that are the indexes of the delims
 --getIndexesHelper
 gIH :: String -> Char -> [Int]
 gIH text delim = indices (Tx.pack $ (:[]) delim) (Tx.pack text)
@@ -21,19 +23,59 @@ getIndexes text delims = gIH text (head delims) ++ getIndexes text (tail delims)
 substring :: Int -> Int -> Text -> Text
 substring start end text = Tx.take (end - start) (Tx.drop start text)
 
+--splitByIndexes: function that returns a list of stings that are each token
 splitByIndexes :: String -> [Int] -> [String]
 splitByIndexes _ [] = []
 splitByIndexes _ [x] = []
 splitByIndexes str lst = Tx.unpack (substring (head lst + 1) (lst !! 1) (Tx.pack str)) : Tx.unpack (substring (lst !! 1) ((lst !! 1) + 1) (Tx.pack str))  : splitByIndexes str (tail lst)
 
+--parseCode: returns a list of strings split by delims, includes delims
 parseCode :: String -> [String]
 parseCode "" = []
 parseCode str = splitByIndexes str ((-1) : sort (getIndexes str splitchars) ++ [length str])
 
+--the dictionary used for translating
 itToEngDict = Map.fromList[
-  ("se", "if"), 
-  ("per", "for")]
+  ("auto","auto"),
+  ("interrompi", "break"),
+  ("caso", "case"),
+  ("cara", "char"),
+  ("carattere", "char"),
+  ("cost", "const"),
+  ("constante", "const"),
+  ("continua", "continue"),
+  ("predefinita", "default"),
+  ("fa", "do"),
+  ("doppio", "double"),
+  ("altro", "else"),
+  ("enum", "enum"),
+  ("enumerazione", "enum"),
+  ("ester", "extern"),
+  ("esterno", "extern"),
+  ("virgola", "float"),
+  ("per", "for"),
+  ("vaa", "goto"),
+  ("se", "if"),
+  ("int", "int"),
+  ("intero", "int"),
+  ("lungo", "long"),
+  ("registro", "register"),
+  ("restituisci", "return"),
+  ("corto", "short"),
+  ("segno", "signed"),
+  ("dimensionedi", "sizeof"),
+  ("statico", "static"),
+  ("strutt", "struct"),
+  ("struttura", "struct"),
+  ("cambria", "switch"),
+  ("tipodef", "typedef"),
+  ("tipodefinizione", "union"),
+  ("senzasegno", "unsigned"),
+  ("vuoto", "void"),
+  ("volatile", "volatile"),
+  ("mentre", "while")]
 
+--the function that helps do the translation based on the map
 translateItToEng :: String -> String
 translateItToEng x = if Map.member x itToEngDict
     then itToEngDict Map.! x
